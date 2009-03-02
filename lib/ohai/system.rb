55fcc956614d56d0c1c2fdfa5439de6dcca0171d
Benjamin Black <bb@joyeuse.local>
2009-03-01 16:39:34 -0800

    [evented] tweaks to refresh_plugins

--- lib/ohai/system.rb
+++ lib/ohai/system.rb
@@ -131,17 +131,17 @@ module Ohai
       refreshments.flatten.uniq
     end
     
-    def refresh_plugins(path)
+    def refresh_plugins(path = "/")
       parts = path.split('/')
-      if parts.length > 0
+      if parts.length == 0
+        h = @providers
+      else
         parts.shift if parts[0].length == 0
         h = @providers
         parts.each do |part|
           break unless h.has_key?(part)
           h = h[part]
         end
-      else
-        h = @providers
       end
 
       refreshments = collect_providers(h)
